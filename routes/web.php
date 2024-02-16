357f2f20bc0031b102f759e62281f9c2a4dc69c6
Nesho99 <gecineven@gmail.com>
2024-02-16 23:26:20 +0100

    registriraj rutu zadataka

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ZadatakController;
 use App\Models\Natjecanje;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NatjecanjeControler;
@@ -22,3 +23,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('natjecanje', NatjecanjeControler::class);
+Route::resource('natjecanje.zadatak',ZadatakController::class)->shallow();
